b385bf3b74133bbf91a5d4562368345d8ae460bb
lokhandeyogesh7 <lokhandeyogesh7@gmail.com>
2020-08-20 22:28:23 +0530

    updated custom UI

--- customizedui/src/main/java/com/yogeshlokhande/customizedui/MainActivity.kt
+++ customizedui/src/main/java/com/yogeshlokhande/customizedui/MainActivity.kt
@@ -1,27 +1,23 @@
-package com.yogeshlokhande.inappmessaging
+package com.yogeshlokhande.customizedui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.inappmessaging.FirebaseInAppMessaging
 import com.google.firebase.inappmessaging.FirebaseInAppMessagingDisplay
 import com.google.firebase.inappmessaging.FirebaseInAppMessagingDisplayCallbacks
 import com.google.firebase.inappmessaging.model.InAppMessage
-import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val firebaseInAppMessageDisplay =
             FirebaseInAppMessagingDisplay { inAppMessage, displayCallbacks ->
-
-                println("fiam yolo "+inAppMessage.title!!.text)
-
                 val alertDialog = AlertDialog.Builder(this@MainActivity)
-                alertDialog.setTitle("Custom UI")
+                alertDialog.setTitle("Custom UI Title :" + inAppMessage.title!!.text)
                 alertDialog.setMessage("Hi I am your custom UI")
                 alertDialog.setCancelable(true)
                 alertDialog.setPositiveButton(
